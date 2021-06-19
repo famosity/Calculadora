e819d2fe875141141f82ab37c3f86c281ffb46ef
Famoso Fernando <gabrielfamoso1996@gmail.com>
2021-06-19 16:37:20 -0300

    test solucionado

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Project by Acevedo Fernando - Ing. Soft. UTN FRRe/i);
+  const linkElement = screen.getByText(/Project by Famoso Fernando - Ing. Soft. UTN FRRe/i);
   expect(linkElement).toBeInTheDocument();
 });
